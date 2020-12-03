53f59fc9e2e02cde4bb998625238be7a58e0ab47
yundowon <70631088+yundowon@users.noreply.github.com>
2020-12-03 15:44:22 +0900

    02

--- main.c
+++ main.c
@@ -2,26 +2,31 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct Book{
+	int number;
+	char title[20];
+};
 
 int main(int argc, char *argv[]) {
 	
-	char *pc = NULL;
-	int i = 0;
+	struct Book *p;
 	
-	pc = (char*)malloc(100*sizeof(char));
-	if(pc == NULL){
+	p = (struct Book*)malloc(2*sizeof(struct Book));
+	
+	if(p == NULL){
 		printf("메모리 할당 오류\n");
-		exit(1);
+		return -1;
 	}
 	
-	for(i=0;i<26;i++)
-	{
-		pc[i] = 'a'+i;
-	}
+	p->number = 1;
+	strcpy(p->title, "C Programming");
 	
-	pc[i] = 0;
-	printf("%s\n", pc);
-	free(pc);
-
+	(p+1)->number = 2;
+	strcpy((p+1)->title, "Electronics");
+	
+	printf("%s %s\n", p->title, (p+1)->title);
+	
+	free(p);
+	return 0;
 }
 
